5163e59a92d11e60e07b32e328aa42e30fa63112
jeffreypaul15 <jeffrey.paul2000@gmail.com>
2021-06-28 07:37:01 +0530

    changes to ball_detection

--- ball_detection/mqtt/client.py
+++ ball_detection/mqtt/client.py
@@ -1,11 +1,7 @@
 import paho.mqtt.client as mqtt
-import time
 
 client = mqtt.Client()
 client.connect('10.147.17.95', 1883)
 
-while True:
-    client.publish("topspin/test", "Ball Detector Connected")
-    time.sleep(1)
-    print("POSE")
-
+client.publish("topspin/test", "Ball Detector Connected")
+print('hi')
