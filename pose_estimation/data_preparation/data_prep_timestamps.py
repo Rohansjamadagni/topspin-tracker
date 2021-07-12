0dd27db4d4b2b8864fbe608a1ba27c0e182c2b81
Kaustubh Kulkarni <kmkapril15@gmail.com>
2021-07-13 04:32:48 +0530

    Added utilities for splitting source video and csv file into multiple based on timestamps

---
+++ pose_estimation/data_preparation/data_prep_timestamps.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+import time
+
+import sys
+sys.path.append('..')
+
+import csv_utils as cu
+from utils import DataSplitter
+from visualise.keypoint_utils import KeypointMapper
+
+import cv2
+import pandas as pd
+import numpy as np
+
+def make_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--video', '-v', default='',
+                        help='Enter path to video')
+
+    parser.add_argument('--csv_file', '-cf', default='',
+                        help='Enter points file')
+    parser.add_argument('--timestamps_file', '-tf', default='',
+                        help='Enter timestamps file')
+
+    parser.add_argument('--csv_output_dir', '-co', default='',
+                        help='Enter stroke directory')
+    parser.add_argument('--video_output_dir', '-vo', default=None, type=str,
+                        help="Saves the output video to provided path")
+
+    parser.add_argument('--stroke_name', default=None, type=str,
+                        help="Provide a single stroke name that has been played for data collection")
+
+    args = parser.parse_args()
+
+    assert args.video != ''
+    assert args.csv_file != ''
+    assert args.timestamps_file != ''
+    assert args.csv_output_dir != ''
+
+    return args
+
+def main():
+    args = make_args()
+
+    splitter = DataSplitter(
+                    video=args.video,
+                    full_keypoint_file=args.csv_file,
+                    timestamps_file=args.timestamps_file)
+
+    splitter.generate_splits(
+        csv_save_dir=args.csv_output_dir,
+        video_save_dir=args.video_output_dir,
+        stroke_name=args.stroke_name
+    )
+
+    print(f"Generated csv splits at: {args.csv_output_dir}")
+    print(f"Generated video splits at: {args.video_output_dir}")
+
+if __name__ == "__main__":
+    main()
---
+++ pose_estimation/data_preparation/utils.py
@@ -0,0 +1,49 @@
+import os
+
+import cv2
+import pandas as pd
+import numpy as np
+
+class DataSplitter:
+    def __init__(
+        self,
+        video,
+        full_keypoint_file,
+        timestamps_file
+    ):
+        self.vid = cv2.VideoCapture(video)
+
+        self.main_df = pd.read_csv(full_keypoint_file, delimiter=',')
+        self.ts_df = pd.read_csv(timestamps_file, delimiter=',')
+
+    def generate_splits(
+        self,
+        csv_save_dir: str = None,
+        video_save_dir: str = None,
+        stroke_name: str = None
+    ):
+        assert csv_save_dir is not None
+
+        if stroke_name is None:
+            stroke_name = 'stroke'
+
+        for idx, (start, stop) in enumerate(zip(self.ts_df['start'], self.ts_df['stop'])):
+            res = self.main_df.loc[(self.main_df['Timestamp'] >= start) & (self.main_df['Timestamp'] <= stop)]
+            res.to_csv(f"{csv_save_dir}/{stroke_name}_{idx}.csv")
+
+            if video_save_dir is not None:
+                out = self.get_writer_object(f"{video_save_dir}/{stroke_name}_{idx}.mp4")
+                for i in res.index.tolist():
+                    self.vid.set(1, i)
+                    ret, frame = self.vid.read()
+                    out.write(frame)
+                out.release()
+
+    def get_writer_object(self, output):
+        vid_fps = self.vid.get(cv2.CAP_PROP_FPS)
+        size = (int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output, fourcc, vid_fps, size)
+
+        return out
