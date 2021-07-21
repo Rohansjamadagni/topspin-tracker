323f98ea885925bb5da3b8a35af6e5efbd875ce7
Kaustubh Kulkarni <kmkapril15@gmail.com>
2021-07-22 01:23:56 +0530

    Integrates vibration with new csv utils file

--- server/pose_utils.py
+++ server/pose_utils.py
@@ -3,12 +3,15 @@ import sys
 import os
 
 import csv_utils as cu
-from vibration_utils import VibrationCSV
+import new_csv_utils as ncu
+# from vibration_utils import VibrationCSV
+
+# keypoints = cu.CSV(filename="keypoint_csvs/test.csv", columns=)
 
 csv_file = f"keypoint_csvs/test.csv"
 cu.initialize_csv(csv_file)
 
-vibration_csv = VibrationCSV(filename="timestamp_csvs/test.csv")
+vibration_csv = CSV(filename="timestamp_csvs/test.csv", columns=['left', 'right'])
 
 def on_connect(contents):
     print("Pose estimation camera connected")
@@ -25,7 +28,8 @@ def on_finish(contents):
     print(f"\n{contents}")
     cu.filter_file(csv_file, hand="right",
                 window_length=5, polyorder=2)
+    vibration_csv.save()
 
 def on_vibration(contents):
     timestamp_list = json.loads(contents)
-    vibration_csv.write_list_to_csv(timestamp_list)
+    vibration_csv.add_list(timestamp_list)
