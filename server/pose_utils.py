befdd1c455253c34c4b20989572b37317c609fcb
Kaustubh Kulkarni <kmkapril15@gmail.com>
2021-07-30 00:43:21 +0530

    Refactors pose cam clients

--- server/pose_utils.py
+++ server/pose_utils.py
@@ -1,39 +1,74 @@
+import paho.mqtt.client as mqtt
+from csv_utils import CSV
 import json
 import sys
 import os
 
-def _get_columns():
-	column_names = ['L-shoulder', 'R-shoulder',
-		   'L-elbow', 'R-elbow',
-		   'L-wrist', 'R-wrist']
+class PoseCam(mqtt.Client):
+    def __init__(
+            self,
+            cam_number: int,
+            csv_dest: str):
 
-	divisions = ['X', 'Y', 'Z']
+        super(PoseCam, self).__init__()
 
-	columns = []
+        self.cam_number = cam_number
+        self.csv_obj = CSV(filename=csv_dest, columns=self._get_columns())
 
-	for column in column_names:
-		for div in divisions:
-			columns.append(f'{column}-{div}')
+    @staticmethod
+    def _get_columns():
+    	column_names = ['L-shoulder', 'R-shoulder',
+    		   'L-elbow', 'R-elbow',
+    		   'L-wrist', 'R-wrist']
 
-	columns.append('Timestamp')
+    	divisions = ['X', 'Y', 'Z']
 
-	return columns
+    	columns = []
 
-def on_connect(contents):
-    print(contents)
+    	for column in column_names:
+    		for div in divisions:
+    			columns.append(f'{column}-{div}')
 
-def on_coords(contents, csv_obj):
-    csv_list = json.loads(contents)
-    csv_obj.add_list(csv_list)
+    	columns.append('Timestamp')
 
-def on_rcv_frame_count(contents):
-    sys.stdout.write(f"\rCurrent frame number: {contents}")
-    sys.stdout.flush()
+    	return columns
 
-def on_finish(contents, csv_obj):
-    print(f"\n{contents}")
+    def on_connect(self, client, userdata, detect_flags, rc):
+        self.subscribe(f"pose_{self.cam_number}/#")
+        print(f"Connected to the pose_{self.cam_number} topic!")
+        print(f"Attempting to connect to pose_{self.cam_number} camera")
 
-    csv_obj.filter_columns(window_length=5, polyorder=2,
-                            columns=kpt_cols[:-1], overwrite=True)
+    def on_message(self, client, ud, msg):
+        contents = msg.payload.decode()
 
-    csv_obj.save()
+        if msg.topic == f'pose_{self.cam_number}/connected':
+            self.__on_connect(contents)
+        elif msg.topic == f'pose_{self.cam_number}/coords':
+            self.__on_coords(contents)
+        elif msg.topic == f'pose_{self.cam_number}/progress':
+            self.__on_rcv_frame_count(contents)
+        elif msg.topic == f'pose_{self.cam_number}/finished':
+            self.__on_finish(contents)
+        elif msg.topic == f'pose_{self.cam_number}/error':
+            pack(contents)
+        else:
+            print(f'Wrong pose_{self.cam_number} topic')
+
+    def __on_connect(self, contents):
+        print(contents)
+
+    def __on_coords(self, contents):
+        csv_list = json.loads(self, contents)
+        self.csv_obj.add_list(csv_list)
+
+    def __on_rcv_frame_count(self, contents):
+        sys.stdout.write(f"\rCurrent frame number: {contents}")
+        sys.stdout.flush()
+
+    def __on_finish(self, contents):
+        print(f"\n{contents}")
+
+        self.csv_obj.filter_columns(window_length=5, polyorder=2,
+                                columns=kpt_cols[:-1], overwrite=True)
+
+        self.csv_obj.save()
--- server/subscriber.py
+++ server/subscriber.py
@@ -4,7 +4,7 @@ import json
 import sys
 import paho.mqtt.client as mqtt
 
-import pose_utils as pu
+from pose_utils import PoseCam
 import ball_utils as bu
 import ansible_utils as au
 import vibration_utils as vu
@@ -20,11 +20,13 @@ keypoint_csv_2 = CSV(filename="keypoint_csvs/cam_2/test.csv", columns=pu._get_co
 
 vib = mqtt.Client()
 ball = mqtt.Client()
-pose_1 = mqtt.Client()
-pose_2 = mqtt.Client()
+
+pose_1 = PoseCame(cam_number=1, csv_dest="keypoint_csvs/cam_1/test.csv")
+pose_2 = PoseCame(cam_number=2, csv_dest="keypoint_csvs/cam_1/test.csv")
 
 vib.connect(broker_ip, broker_port)
 ball.connect(broker_ip, broker_port)
+
 pose_1.connect(broker_ip, broker_port)
 pose_2.connect(broker_ip, broker_port)
 
@@ -40,54 +42,10 @@ def on_connect_ball(client, userdata, detect_flags, rc):
     print("Connected to the ball topic!")
     print("Attempting to connect to ball camera")
 
-def on_connect_pose_1(client, userdata, detect_flags, rc):
-    client.subscribe("pose_1/#")
-    print("Connected to the pose_1 topic!")
-    print("Attempting to connect to pose_1 camera")
-    # au.ansible()
-
-def on_connect_pose_2(client, userdata, detect_flags, rc):
-    client.subscribe("pose_2/#")
-    print("Connected to the pose_2 topic!")
-    print("Attempting to connect to pose_2 camera")
-    # au.ansible()
-
 def on_connect_vib(client, userdata, detect_flags, rc):
     client.subscribe("vibration/#")
     print("Connected to the vibration topic!")
     print("Attempting to connect to sensors")
-
-def on_message_pose_1(client, ud, msg):
-    contents = msg.payload.decode()
-
-    if msg.topic == 'pose_1/connected':
-        pu.on_connect(contents)
-    elif msg.topic == 'pose_1/coords':
-        pu.on_coords(contents)
-    elif msg.topic == 'pose_1/progress':
-        pu.on_rcv_frame_count(contents)
-    elif msg.topic == 'pose_1/vibration':
-        pu.on_vibration(contents)
-    elif msg.topic == 'pose_1/finished':
-        pu.on_finish(contents)
-    elif msg.topic == 'pose_1/error':
-        pack(contents)
-    else:
-        print('Wrong pose_1 topic')
-
-def on_message_pose_2(client, ud, msg):
-    contents = msg.payload.decode()
-
-    if msg.topic == 'pose_2/connected':
-        pu.on_connect(contents)
-    elif msg.topic == 'pose_2/coords':
-        pu.on_coords(contents)
-    elif msg.topic == 'pose_2/finished':
-        pu.on_finish(contents)
-    elif msg.topic == 'pose_2/error':
-        pack(contents)
-    else:
-        print('Wrong pose_2 topic')
 
 def on_message_vib(client, ud, msg):
     contents = msg.payload.decode()
@@ -129,14 +87,9 @@ while True:
         vib.on_connect = on_connect_vib
         vib.on_message = on_message_vib
 
-        pose_1.on_connect = on_connect_pose_1
-        pose_1.on_message = on_message_pose_1
-
-        pose_2.on_connect = on_connect_pose_2
-        pose_2.on_message = on_message_pose_2
-
         # ball.loop_start()
         vib.loop_start()
+
         pose_1.loop_start()
         pose_2.loop_start()
     except KeyboardInterrupt:
