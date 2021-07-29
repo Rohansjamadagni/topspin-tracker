6f8af19a3d3cb21aa181196476ee0e3add34d8ea
Kaustubh Kulkarni <kmkapril15@gmail.com>
2021-07-30 01:23:19 +0530

    combines all utils, makes everything object based

---
+++ server/server_utils.py
@@ -0,0 +1,193 @@
+import paho.mqtt.client as mqtt
+from csv_utils import CSV
+import json
+import sys
+import os
+
+class Ansible:
+    def __init__(self):
+        print("Initialising...")
+        os.system('ansible-playbook -i ../ansible/hosts ../ansible/init.yml')
+
+    @staticmethod
+    def destroy(error=None):
+        if error is not None:
+            print(error)
+        print("PACK BRO")
+        os.system('ansible-playbook -i ../ansible/hosts ../ansible/packed.yml')
+        exit()
+
+    @staticmethod
+    def ping(device):
+        command = 'ansible all -m ping -v'
+        p = subprocess.check_output(command.split(' '))
+
+    @staticmethod
+    def start_record():
+        os.system('ansible-playbook -i ../ansible/hosts ../ansible/control.yml  &')
+
+    @staticmethod
+    def start_replay():
+        os.system('ansible-playbook -i ../ansible/hosts ../ansible/replay.yml  &')
+
+    @staticmethod
+    def take_picture():
+        os.system('ansible-playbook -i ../ansible/hosts.yml ../ansible/picture.yml')
+
+class PoseCam(mqtt.Client, Ansible):
+    def __init__(
+            self,
+            cam_number: int,
+            csv_dest: str):
+
+        super(PoseCam, self).__init__()
+
+        self.cam_number = cam_number
+        self.csv_obj = CSV(filename=csv_dest, columns=self._get_columns())
+
+    @staticmethod
+    def _get_columns():
+        column_names = ['L-shoulder', 'R-shoulder',
+               'L-elbow', 'R-elbow',
+               'L-wrist', 'R-wrist']
+
+        divisions = ['X', 'Y', 'Z']
+
+        columns = []
+
+        for column in column_names:
+            for div in divisions:
+                columns.append(f'{column}-{div}')
+
+        columns.append('Timestamp')
+
+        return columns
+
+    def on_connect(self, client, userdata, detect_flags, rc):
+        self.subscribe(f"pose_{self.cam_number}/#")
+        print(f"Connected to the pose_{self.cam_number} topic!")
+        print(f"Attempting to connect to pose_{self.cam_number} camera")
+
+    def on_message(self, client, ud, msg):
+        contents = msg.payload.decode()
+
+        if msg.topic == f'pose_{self.cam_number}/connected':
+            self.__on_connect(contents)
+        elif msg.topic == f'pose_{self.cam_number}/coords':
+            self.__on_coords(contents)
+        elif msg.topic == f'pose_{self.cam_number}/progress':
+            self.__on_rcv_frame_count(contents)
+        elif msg.topic == f'pose_{self.cam_number}/finished':
+            self.__on_finish(contents)
+        elif msg.topic == f'pose_{self.cam_number}/error':
+            self.destroy(contents)
+        else:
+            print(f'Wrong pose_{self.cam_number} topic')
+
+    def __on_connect(self, contents):
+        print(contents)
+
+    def __on_coords(self, contents):
+        csv_list = json.loads(self, contents)
+        self.csv_obj.add_list(csv_list)
+
+    def __on_rcv_frame_count(self, contents):
+        sys.stdout.write(f"\rCurrent frame number: {contents}")
+        sys.stdout.flush()
+
+    def __on_finish(self, contents):
+        print(f"\n{contents}")
+
+        self.csv_obj.filter_columns(window_length=5, polyorder=2,
+                                columns=kpt_cols[:-1], overwrite=True)
+
+        self.csv_obj.save()
+
+class BallCam(mqtt.Client, Ansible):
+    def __init__(
+        self,
+        cam_number: int):
+
+        super(BallCam, self).__init__()
+
+        self.cam_number = cam_number
+
+    def on_connect(self, client, userdata, detect_flags, rc):
+        self.subscribe(f"ball_{self.cam_number}/#")
+        print(f"Connected to the ball_{self.cam_number} topic!")
+        print(f"Attempting to connect to ball_{self.cam_number} camera")
+
+    def on_message(self, client, ud, msg):
+        contents = msg.payload.decode()
+
+        if msg.topic == f'ball_{self.cam_number}/connected':
+            self.__on_connect(contents)
+        elif msg.topic == f'ball_{self.cam_number}/progress/record':
+            self.__on_record(contents)
+        elif msg.topic == f'ball_{self.cam_number}/record/finished':
+            self.__on_finish_record(contents)
+        elif msg.topic == f'ball_{self.cam_number}/progress/replay':
+            self.__on_replay(contents)
+        # receive data from ansible to start replay
+        elif msg.topic == f'ball_{self.cam_number}/replay/finished':
+            self.__on_finish_replay(contents)
+        elif msg.topic == f'ball_{self.cam_number}/error':
+            self.destroy(contents)
+        else:
+            print(f'Wrong ball_{self.cam_number} topic')
+
+    def __on_connect(contents):
+        pass
+
+    def __on_record(contents):
+        pass
+
+    def __on_finish_record(contents):
+        pass
+
+    def __on_replay(contents):
+        pass
+
+    def __on_finish_replay(contents):
+        pass
+
+class Vibrator(mqtt.Client, Ansible):
+    def __init__(
+        self,
+        cam_number: int,
+        csv_dest: str):
+
+        super(Vibrator, self).__init__()
+
+        self.cam_number = cam_number
+        self.csv_obj = CSV(filename=csv_dest, columns=['left', 'right'])
+
+    def on_connect(self, client, userdata, detect_flags, rc):
+        self.subscribe(f"vibration_{self.cam_number}/#")
+        print(f"Connected to the vibration_{self.cam_number} topic!")
+        print(f"Attempting to connect to vibration_{self.cam_number} camera")
+
+    def on_message_vib(self, client, ud, msg):
+        contents = msg.payload.decode()
+
+        if msg.topic == f'vibration_{self.cam_number}/connected':
+            self.__on_connect(contents)
+        elif msg.topic == f'vibration_{self.cam_number}/pitch':
+            self.__on_vibration(contents)
+        elif msg.topic == f'vibration_{self.cam_number}/finished':
+            self.__on_finish(contents)
+        elif msg.topic == f'vibration_{self.cam_number}/error':
+            pack(contents)
+        else:
+            print(f'Wrong vibration_{self.cam_number} topic')
+
+    def __on_connect(contents):
+        print(contents)
+
+    def __on_vibration(contents):
+        timestamp_list = json.loads(contents)
+        self.csv_obj.add_list(timestamp_list)
+
+    def __on_finish(contents):
+        print(contents)
+        self.csv_obj.save()
--- server/subscriber.py
+++ server/subscriber.py
@@ -2,27 +2,17 @@ import os
 import subprocess
 import json
 import sys
-import paho.mqtt.client as mqtt
 
-from pose_utils import PoseCam
-import ball_utils as bu
-import ansible_utils as au
-import vibration_utils as vu
-
-from csv_utils import CSV
+from server_utils import PoseCam, BallCam, Vibrator, Ansible
 
 broker_config = json.load(open('../ips.json', 'r'))
 broker_ip = broker_config['broker_ip']
 broker_port = broker_config['broker_port']
 
-keypoint_csv_1 = CSV(filename="keypoint_csvs/cam_1/test.csv", columns=pu._get_columns())
-keypoint_csv_2 = CSV(filename="keypoint_csvs/cam_2/test.csv", columns=pu._get_columns())
-
-vib = mqtt.Client()
-ball = mqtt.Client()
-
-pose_1 = PoseCame(cam_number=1, csv_dest="keypoint_csvs/cam_1/test.csv")
-pose_2 = PoseCame(cam_number=2, csv_dest="keypoint_csvs/cam_1/test.csv")
+vib = Vibrator(cam_number=1, csv_dest="timestamp_csvs/test.csv")
+ball = BallCam(cam_number=1)
+pose_1 = PoseCam(cam_number=1, csv_dest="keypoint_csvs/cam_1/test.csv")
+pose_2 = PoseCam(cam_number=2, csv_dest="keypoint_csvs/cam_1/test.csv")
 
 vib.connect(broker_ip, broker_port)
 ball.connect(broker_ip, broker_port)
@@ -30,72 +20,12 @@ ball.connect(broker_ip, broker_port)
 pose_1.connect(broker_ip, broker_port)
 pose_2.connect(broker_ip, broker_port)
 
-def pack(error=None):
-    if error is not None:
-        print(error)
-    print("PACK BRO")
-    au.ansible_destroy()
-    exit()
-
-def on_connect_ball(client, userdata, detect_flags, rc):
-    client.subscribe("ball/#")
-    print("Connected to the ball topic!")
-    print("Attempting to connect to ball camera")
-
-def on_connect_vib(client, userdata, detect_flags, rc):
-    client.subscribe("vibration/#")
-    print("Connected to the vibration topic!")
-    print("Attempting to connect to sensors")
-
-def on_message_vib(client, ud, msg):
-    contents = msg.payload.decode()
-
-    if msg.topic == 'vibration/connected':
-        vu.on_connect(contents)
-    elif msg.topic == 'vibration/pitch':
-        vu.on_vibration(contents)
-    elif msg.topic == 'vibration/finished':
-        vu.on_finish(contents)
-    elif msg.topic == 'vibration/error':
-        pack(contents)
-    else:
-        print('Wrong pose_1 topic')
-
-def on_message_ball(client, ud, msg):
-    contents = msg.payload.decode()
-
-    if msg.topic == 'ball/connected':
-        bu.on_connect(contents)
-    elif msg.topic == 'ball/progress/record':
-        bu.on_record(contents)
-    elif msg.topic == 'ball/record/finished':
-        bu.on_finish_record(contents)
-    elif msg.topic == 'ball/progress/replay':
-        bu.on_replay(contents)
-    # receive data from ansible to start replay
-    elif msg.topic == 'ball/replay/finished':
-        bu.on_finish_replay(contents)
-    elif msg.topic == 'ball/error':
-        pack(contents)
-    else:
-        print('Wrong ball topic')
-
 while True:
-    # ball.on_connect = on_connect_ball
-    # ball.on_message = on_message_ball
     try:
-        vib.on_connect = on_connect_vib
-        vib.on_message = on_message_vib
-
-        # ball.loop_start()
+        ball.loop_start()
         vib.loop_start()
 
         pose_1.loop_start()
         pose_2.loop_start()
     except KeyboardInterrupt:
-        # print("Press r top stop recording, Press q to quit:")
-        # inp = input()
-        # if(inp == 'r'):
-        #     au.stat_replay()
-        # else:
         exit()
